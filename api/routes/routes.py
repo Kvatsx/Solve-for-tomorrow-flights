2e94986627a2aa56d2c0c7920b6885cc63bdc2c5
rupav <rupavwinchester@gmail.com>
2020-06-29 16:20:09 +0530

    Add procfile

--- api/routes/routes.py
+++ api/routes/routes.py
@@ -9,6 +9,7 @@ CORS(mod)
 @mod.route('/', methods=['GET'])
 def index():
     user_collection = mongo.db.discount
-    post = {"_id": 0, "name": "mmt"}
-    user_collection.insert(post)
-    return "Post Added!", 201
+    # post = {"_id": 0, "name": "mmt"}
+    # user_collection.insert(post)
+    pilot = user_collection.find_one({"name": "mmt"})
+    return jsonify({'pilot': pilot}), 201
