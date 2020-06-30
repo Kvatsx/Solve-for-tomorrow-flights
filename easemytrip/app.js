bfa2ad41e57db0e8b549faee9de391b3e75945c9
Gaurav Tiwari <tiwarigaurav1998@gmail.com>
2020-06-30 23:04:34 +0530

    Setup express rest api

--- easemytrip/app.js
+++ easemytrip/app.js
@@ -2,10 +2,11 @@ const fetch = require("node-fetch");
 const MongoClient = require('mongodb').MongoClient
 const DB=require('./config.json').DB
 const fs = require('fs');
+const app=require('express')();
 
 let flightcollection=null;
 
-async function getFlights() {
+async function getFlights(req_data) {
 
     let response = await fetch("https://flightservice.easemytrip.com/EmtAppService/AirAvail_Lights/AirSearchLightFromCloud", {
         "headers": {
@@ -21,7 +22,7 @@ async function getFlights() {
       },
       "referrer": "https://flight.easemytrip.com/FlightList/Index?srch=DEL-Delhi-India|BOM-Mumbai-India|07/07/2020&px=1-0-0&cbn=0&ar=undefined&isow=true&isdm=true&lng=&CouponCode=",
       "referrerPolicy": "no-referrer-when-downgrade",
-      "body": "{\"org\":\"DEL\",\"dept\":\"BOM\",\"adt\":\"1\",\"chd\":\"0\",\"inf\":\"0\",\"deptDT\":\"2020-07-07\",\"arrDT\":null,\"isDomestic\":\"true\",\"isOneway\":true,\"airline\":\"undefined\",\"Cabin\":\"0\",\"currCode\":\"INR\",\"appType\":1,\"isSingleView\":false,\"ResType\":1,\"CouponCode\":\"\",\"IpAddress\":\"\",\"userid\":\"\",\"UUID\":\"\"}",
+      "body": "{\"org\":\"DEL\",\"dept\":\"HYD\",\"adt\":\"1\",\"chd\":\"0\",\"inf\":\"0\",\"deptDT\":\"2020-07-07\",\"arrDT\":null,\"isDomestic\":\"true\",\"isOneway\":true,\"airline\":\"undefined\",\"Cabin\":\"0\",\"currCode\":\"INR\",\"appType\":1,\"isSingleView\":false,\"ResType\":1,\"CouponCode\":\"\",\"IpAddress\":\"\",\"userid\":\"\",\"UUID\":\"\"}",
       "method": "POST",
       "mode": "cors",
       "credentials": "omit"
@@ -29,10 +30,13 @@ async function getFlights() {
     console.log(response.status + " " + response.statusText)
 
     let data = await response.json()
+
     return data;
 }
 
-function showRelevantInfo(data, db) {
+function showRelevantInfo(data) {
+    // console.log(data);
+
     var locationDetails = data.A;
     var flightNames = data.C;
     var locationAndCountryMap = data.Cnty;
@@ -54,6 +58,8 @@ function showRelevantInfo(data, db) {
     for (flight in data.j[0].s) {
         var obj = {};
         obj['flightName'] = flightNames[data.dctFltDtl[data.j[0].s[flight].b[0].FL[0]].AC];
+        obj['departureDate']= data.SQ[0].deptDT
+        obj['arrivalDate']= data.SQ[0].arrDT
         obj['depart'] = data.dctFltDtl[data.j[0].s[flight].b[0].FL[0]].DTM;
         obj['duration'] = data.j[0].s[flight].b[0].JyTm;
         obj['arrive'] = data.dctFltDtl[data.j[0].s[flight].b[0].FL.length-1].ATM;
@@ -66,27 +72,57 @@ function showRelevantInfo(data, db) {
 
         docs.push(obj);
     }
-    // console.log(flightList);
+
     console.log(docs);
     console.log(docs.length)
-    const flightData = JSON.stringify(flightList, null, 4);
 
-    db.insertMany(docs,function(err,res){
-        if(err)
-            throw err;
-        console.log("Saved to DB!");
-    })
-
+    
+    return docs;
 }
 
 
-MongoClient.connect(DB.URI, function(err,db){
+
+
+
+app.get('/getFlights',function(req,res){
+    MongoClient.connect(DB.URI, function(err,db){
     if(err)
         throw err;
 
     flightcollection=db.db('test').collection('easemytrip_node');
-    getFlights().then(data => {
-        showRelevantInfo(data, flightcollection);
-    });
-
+    getFlights(req.query).then(function(data){
+        let docs= showRelevantInfo(data);
+        flightcollection.insertMany(docs).then(result=>{
+            console.log("Saved to DB!");
+            res.send("Result saved in DB!");
+        }).catch(err=>{
+            res.send(err);
+        });
+    })
+    })  
 })
+
+app.get('/',function(req,res){
+    res.send('Hey there!')
+})
+
+app.listen(2345, ()=>{
+    console.log('Connected to node server');
+    
+})
+
+//JSON request parameter format
+// {
+//     "origin": "DEL",
+//     "destination":"BOM",
+//     "departureDate":"2020-07-07",
+//     "arrivalDate": null,
+//     "adults":"1",
+//     "children":"0",
+//     "infants":"0",
+//     "isDomestic":"true",
+//     "isOneway":"true",
+//     "airline":"undefined",
+//     "cabin":"0",
+//     "currencyCode":"INR",
+// }
