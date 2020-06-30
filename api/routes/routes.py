237a78db294514171f0c0b51b9ec7f92172457c7
kaustavvats <kaustav16048@iiitd.ac.in>
2020-06-30 14:38:59 +0530

    added easemytrip data mongo connection

--- api/routes/routes.py
+++ api/routes/routes.py
@@ -2,14 +2,27 @@ from flask import Blueprint, jsonify, request
 from flask_cors import CORS
 
 from api.database import mongo
+import json
+
 
 mod = Blueprint('main', __name__)
 CORS(mod)
 
 @mod.route('/', methods=['GET'])
 def index():
+    # insertData()
     user_collection = mongo.db.discount
-    # post = {"_id": 0, "name": "mmt"}
-    # user_collection.insert(post)
-    pilot = user_collection.find_one({"name": "mmt"})
-    return jsonify({'pilot': pilot}), 201
+    data = user_collection['easemytrip'].find()
+    data = list(data)
+    del data[0]['_id']
+    return jsonify(data[0]), 201
+
+
+# test function to load data in mongodb
+def insertData():
+    # update path of data.json before using this function
+    f = open('./easemytrip/data.json')
+    data = json.load(f)
+    user_collection = mongo.db.discount
+    user_collection['easemytrip'].drop()
+    user_collection['easemytrip'].insert(data)
