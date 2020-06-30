25d36027d3bf493b6935b220b18ba02f54fa5934
kaustavvats <kaustav16048@iiitd.ac.in>
2020-06-30 20:54:37 +0530

    updated api link

--- api/routes/routes.py
+++ api/routes/routes.py
@@ -7,9 +7,31 @@ import json
 
 mod = Blueprint('main', __name__)
 CORS(mod)
-
-@mod.route('/', methods=['GET'])
-def index():
+# /easemytrip/from=<fr>&to=<to>&dd=<dd>&rd=<rd>&tvlr=<tvlr>&cl=<cl>
+@mod.route('/easemytrip/from=<fr>&to=<to>&dd=<dd>&rd=<rd>&tvlr=<tvlr>&cl=<cl>', methods=['GET'])
+# def index(fr, to, dd):
+def index(fr, to, dd, rd, tvlr, cl):
+    obj = {}
+    if fr == "" or to == "" or dd == "" or len(tvlr) != 5 or cl == "":
+        return jsonify({
+            'status':404, 'message': "Incorrect api call",
+            'exampleAPI': "/easemytrip/from=DEL-Delhi-India&to=BOM-Mumbai-India&dd=30-07-2020&rd=null&tvlr=1-0-0&cl=0"
+        }), 404
+    try :
+        obj["from"] = [fr, fr.split('-')[0]]
+        obj["to"] = [to, to.split('-')[0]]
+        obj["dd"] = dd.replace('-', '/')
+        obj["rd"] = rd.replace('-', '/')
+        obj["tvlr"] = {"v": tvlr, "adt": tvlr.split('-')[0], "chd": tvlr.split('-')[1], "inf": tvlr.split('-')[2]}
+        obj["cl"] = cl
+        print(obj)
+        # TODO: Send this json object to App.js
+    except:
+        return jsonify({
+            'status':404, 'message': "Incorrect api call",
+            'exampleAPI': "/easemytrip/from=DEL-Delhi-India&to=BOM-Mumbai-India&dd=30-07-2020&rd=null&tvlr=1-0-0&cl=0"
+        }), 404
+        
     # insertData()
     user_collection = mongo.db.discount
     data = user_collection['easemytrip'].find()
@@ -26,3 +48,4 @@ def insertData():
     user_collection = mongo.db.discount
     user_collection['easemytrip'].drop()
     user_collection['easemytrip'].insert(data)
+# https://flight.easemytrip.com/FlightList/Index?srch=DEL-Delhi-India|BOM-Mumbai-India|30/07/2020&px=1-0-0&cbn=0&ar=undefined&isow=true&isdm=true&lng=&
